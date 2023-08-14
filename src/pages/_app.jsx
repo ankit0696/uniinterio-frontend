5741fa66abf81d15ee49e6c1f0e9eeaba38e7b46
Ankit Kumar <ankitkr0696@gmail.com>
2023-08-15 00:32:11 +0530

    add g-analytics

---
+++ src/pages/_app.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from 'react'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+import Script from 'next/script'
+
+function usePrevious(value) {
+  let ref = useRef()
+
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+
+  return ref.current
+}
+
+export default function App({ Component, pageProps, router }) {
+  let previousPathname = usePrevious(router.pathname)
+
+  return (
+    <>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-RR28LWZF11"
+          ></Script>
+          <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-RR28LWZF11');
+        `}
+          </Script>
+          <Component previousPathname={previousPathname} {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
