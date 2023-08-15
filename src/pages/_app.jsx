d345b3bf812732ac4980c3ded2009340eb948904
Ankit Kumar <ankitkr0696@gmail.com>
2023-08-15 15:04:14 +0530

    add vercel analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -47,6 +48,7 @@ export default function App({ Component, pageProps, router }) {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </>
   )
 }
