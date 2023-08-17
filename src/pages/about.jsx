156567332cf0785eafd9e693bdf0149ee008bfcd
Ankit Kumar <ankitkr0696@gmail.com>
2023-08-17 09:46:29 +0530

    update open social in new tab

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -17,6 +17,8 @@ function SocialLink({ className, href, children, icon: Icon }) {
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
+        target="_blank"
+        rel="noopener noreferrer"
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-rose-500 dark:text-zinc-200 dark:hover:text-rose-500"
       >
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-rose-500" />
